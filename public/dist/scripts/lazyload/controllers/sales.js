c0710a8bfa3bce69bbf029d6dc1ea7e90f734049
Erick J. Pac <epac@researchmobile.co>
2017-05-22 14:36:25 -0600

    print in sales view

--- public/dist/scripts/lazyload/controllers/sales.js
+++ public/dist/scripts/lazyload/controllers/sales.js
@@ -235,7 +235,7 @@
                 }
                 var data = {};
                 data.data_sale = angular.toJson(data_sale);
-
+                //$scope.modalPrintSale();
                 SalesService.saveSale(data).then(
                     function successCallback(response) {
                         if (response.data.result) {
@@ -270,21 +270,39 @@
                 modal.close();
                 var printContents = document.getElementById("imprimir-seccion").innerHTML;
                 var popupWin = window.open('', '_blank', 'width=350,height=400');
-                var style =
+                /*var style =
                     "<style>" +
                     "html{ font-size: 14px; font-family: sans-serif !important; }"+
                     "body{ width: 300px; }"+
                     "p{ margin: 0px !important; padding: 0px; }"+
                     "small{ font-size: 12px; }"+
                     "imprimir-seccion{ padding:5px; margin:5px; }"+
-                    "</style>";
+                    "</style>";*/
+                var style = "";
                 popupWin.document.open();
                 popupWin.document.write('<html><head>'+style+'</head><body onload="window.print()">' + printContents + '</body></html>');
                 popupWin.document.close();
             };
 
             $scope.modalPrintSale = function() {
-                $scope.dataModal = saleModal;
+                //$scope.dataModal = saleModal;
+                console.log(saleModal);
+                $scope.dataModal = {};
+                $scope.dataModal.correlative = saleModal.correlative;
+                $scope.dataModal.customer_name = saleModal.customer.name;
+                $scope.dataModal.customer_direction = saleModal.customer.direction;
+                $scope.dataModal.customer_nit = saleModal.customer.nit;
+                $scope.dataModal.date = $filter('date')(new Date(saleModal.date),'dd/MM/yyyy');
+                $scope.dataModal.total = $filter('number')(saleModal.total, 2);
+                $scope.dataModal.letters = NumeroALetras($scope.dataModal.total);
+                var detalles = 9 - saleModal.products.length;
+                $scope.dataModal.detail = [];
+                angular.forEach(saleModal.products, function(value, key){
+                    $scope.dataModal.detail.push(value);
+                });
+                for(var i = 0; i < detalles; i++) {
+                    $scope.dataModal.detail.push({subtotal:"|"});
+                }
 
                 modal = $modal.open({
                     templateUrl: 'views/app/sales-modal.html',
@@ -344,7 +362,166 @@
 
             $scope.modalClose = function() {
                 modal.close();
-            }
+            };
+
+            function Unidades(num){
+
+                switch(num)
+                {
+                    case 1: return "UN";
+                    case 2: return "DOS";
+                    case 3: return "TRES";
+                    case 4: return "CUATRO";
+                    case 5: return "CINCO";
+                    case 6: return "SEIS";
+                    case 7: return "SIETE";
+                    case 8: return "OCHO";
+                    case 9: return "NUEVE";
+                }
+
+                return "";
+            }//Unidades()
+
+            function Decenas(num){
+
+                var decena = Math.floor(num/10);
+                var unidad = num - (decena * 10);
+
+                switch(decena)
+                {
+                    case 1:
+                        switch(unidad)
+                        {
+                            case 0: return "DIEZ";
+                            case 1: return "ONCE";
+                            case 2: return "DOCE";
+                            case 3: return "TRECE";
+                            case 4: return "CATORCE";
+                            case 5: return "QUINCE";
+                            default: return "DIECI" + Unidades(unidad);
+                        }
+                    case 2:
+                        switch(unidad)
+                        {
+                            case 0: return "VEINTE";
+                            default: return "VEINTI" + Unidades(unidad);
+                        }
+                    case 3: return DecenasY("TREINTA", unidad);
+                    case 4: return DecenasY("CUARENTA", unidad);
+                    case 5: return DecenasY("CINCUENTA", unidad);
+                    case 6: return DecenasY("SESENTA", unidad);
+                    case 7: return DecenasY("SETENTA", unidad);
+                    case 8: return DecenasY("OCHENTA", unidad);
+                    case 9: return DecenasY("NOVENTA", unidad);
+                    case 0: return Unidades(unidad);
+                }
+            }//Unidades()
+
+            function DecenasY(strSin, numUnidades) {
+                if (numUnidades > 0)
+                    return strSin + " Y " + Unidades(numUnidades)
+
+                return strSin;
+            }//DecenasY()
+
+            function Centenas(num) {
+                var centenas = Math.floor(num / 100);
+                var decenas = num - (centenas * 100);
+
+                switch(centenas)
+                {
+                    case 1:
+                        if (decenas > 0)
+                            return "CIENTO " + Decenas(decenas);
+                        return "CIEN";
+                    case 2: return "DOSCIENTOS " + Decenas(decenas);
+                    case 3: return "TRESCIENTOS " + Decenas(decenas);
+                    case 4: return "CUATROCIENTOS " + Decenas(decenas);
+                    case 5: return "QUINIENTOS " + Decenas(decenas);
+                    case 6: return "SEISCIENTOS " + Decenas(decenas);
+                    case 7: return "SETECIENTOS " + Decenas(decenas);
+                    case 8: return "OCHOCIENTOS " + Decenas(decenas);
+                    case 9: return "NOVECIENTOS " + Decenas(decenas);
+                }
+
+                return Decenas(decenas);
+            }//Centenas()
+
+            function Seccion(num, divisor, strSingular, strPlural) {
+                var cientos = Math.floor(num / divisor);
+                var resto = num - (cientos * divisor);
+
+                var letras = "";
+
+                if (cientos > 0)
+                    if (cientos > 1)
+                        letras = Centenas(cientos) + " " + strPlural;
+                    else
+                        letras = strSingular;
+
+                if (resto > 0)
+                    letras += "";
+
+                return letras;
+            }//Seccion()
+
+            function Miles(num) {
+                var divisor = 1000;
+                var cientos = Math.floor(num / divisor);
+                var resto = num - (cientos * divisor);
+
+                var strMiles = Seccion(num, divisor, "UN MIL", "MIL");
+                var strCentenas = Centenas(resto);
+
+                if(strMiles == "")
+                    return strCentenas;
+
+                return strMiles + " " + strCentenas;
+            }//Miles()
+
+            function Millones(num) {
+                var divisor = 1000000;
+                var cientos = Math.floor(num / divisor);
+                var resto = num - (cientos * divisor);
+
+                var strMillones = Seccion(num, divisor, "UN MILLON DE", "MILLONES DE");
+                var strMiles = Miles(resto);
+
+                if(strMillones == "")
+                    return strMiles;
+
+                return strMillones + " " + strMiles;
+            }//Millones()
+
+            function NumeroALetras(num) {
+                var data = {
+                    numero: num,
+                    enteros: Math.floor(num),
+                    centavos: (((Math.round(num * 100)) - (Math.floor(num) * 100))),
+                    letrasCentavos: "",
+                    letrasMonedaPlural: 'QUETZALES',//“PESOS”, 'Dólares', 'Bolívares', 'etcs'
+                    letrasMonedaSingular: 'QUETZAL', //“PESO”, 'Dólar', 'Bolivar', 'etc'
+
+                    letrasMonedaCentavoPlural: "CENTAVOS",
+                    letrasMonedaCentavoSingular: "CENTAVO"
+                };
+
+                if (data.centavos > 0) {
+                    data.letrasCentavos = "CON " + (function (){
+                            if (data.centavos == 1)
+                                return Millones(data.centavos) + " " + data.letrasMonedaCentavoSingular;
+                            else
+                                return Millones(data.centavos) + " " + data.letrasMonedaCentavoPlural;
+                        })();
+                };
+
+                if(data.enteros == 0)
+                    return "CERO " + data.letrasMonedaPlural + " " + data.letrasCentavos;
+                if (data.enteros == 1)
+                    return Millones(data.enteros) + " " + data.letrasMonedaSingular + " " + data.letrasCentavos;
+                else
+                    return Millones(data.enteros) + " " + data.letrasMonedaPlural + " " + data.letrasCentavos;
+            }//NumeroALetras()
         }])
 
         .directive('myEnter', function () {
